8470ba052a1515fc2f67d1b64f9d0e0a4b1626ab
SherAndrei <welldryus@gmail.com>
2025-02-26 15:13:36 +0300

    use pure prompt
    agnoster could not be displayed correctly in vscode
    source: https://github.com/microsoft/vscode/issues/28304

--- scripts/generate-dotfiles-readme.zsh
+++ scripts/generate-dotfiles-readme.zsh
@@ -4,8 +4,10 @@ echo "# Dotfiles"
 echo
 
 scripts/md-tree.zsh \
+    --exclude .gitmodules \
     --exclude .pre-commit-config.yaml \
     --exclude .stow-local-ignore \
     --exclude README.md \
+    --exclude-dir .config/zsh.d/pure \
     --exclude-dir .git \
     --exclude-dir scripts \
