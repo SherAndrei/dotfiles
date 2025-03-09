bfe4ba97a08b0e704d834310f1d15090ec4cc209
SherAndrei <welldryus@gmail.com>
2025-03-09 22:29:31 +0300

    prompt.zsh: use extended ascii symbols

--- .config/zsh.d/prompt.zsh
+++ .config/zsh.d/prompt.zsh
@@ -3,6 +3,12 @@
 
 fpath+=( "$ZSH_CONFIG_DIR"/pure )
 
+# extended ascii symbols instead of default UTF-8
+# source: https://github.com/sindresorhus/pure/wiki/Customizations,-hacks-and-tweaks#safer-symbols
+PURE_PROMPT_SYMBOL='»'
+PURE_GIT_DOWN_ARROW='↓'
+PURE_GIT_UP_ARROW='↑'
+
 autoload -U promptinit;
 promptinit
 
