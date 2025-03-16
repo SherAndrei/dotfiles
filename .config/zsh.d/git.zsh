7404b2b9964861582159937db92b263d481a1cbc
SherAndrei <welldryus@gmail.com>
2025-03-17 01:25:44 +0300

    git.zsh: use command instead of 'private' variable to access system git

--- .config/zsh.d/git.zsh
+++ .config/zsh.d/git.zsh
@@ -2,14 +2,13 @@
 # wsl's git is slow on windows filesystem
 # source: https://github.com/microsoft/WSL/issues/4401
 
-__git=$(which git)
 function git() {
   autoload -U is_wsl
 
   if is_wsl && [[ "${PWD}" =~ "/mnt/\w/" ]]; then
     git.exe "$@"
   else
-    "${__git}" "$@"
+    command git "$@"
   fi
 }
 
