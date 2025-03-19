ddb8258bd4be8fa270bb381f3af4df7553742940
SherAndrei <welldryus@gmail.com>
2025-03-19 04:33:23 +0300

    directories.zsh: add fuzzy cd

--- .config/zsh.d/directories.zsh
+++ .config/zsh.d/directories.zsh
@@ -3,3 +3,9 @@
 setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
+
+cf() {
+    local dir=$(find -L ${1:-.} -mindepth 1 -path "*/\.git" -prune -o -type d -print |
+        fzf --height 40% --reverse --preview="tree -CN {}")
+    [[ -z $dir ]] || { print -s cd $dir ; cd $dir }
+}
