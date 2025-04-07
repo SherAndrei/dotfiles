dbd1d44528fed21bf889b4ad474fd490e88fbfe1
SherAndrei <welldryus@gmail.com>
2025-04-07 17:18:04 +0300

    ls.sh: sort dotfiles first

--- .config/bash.d/ls.sh
+++ .config/bash.d/ls.sh
@@ -2,5 +2,12 @@
 # source: https://www.gnu.org/software/coreutils/manual/html_node/ls-invocation.html
 
 alias ls='ls --color=auto'
-alias l='ls  -l --human-readable --almost-all'
-alias ll='ls -l --human-readable'
+
+LS_OPTIONS=""
+
+LS_OPTIONS="${LS_OPTIONS} -l" # use a long listing format
+LS_OPTIONS="${LS_OPTIONS} --human-readable" # print sizes like 1K 234M 2G etc.
+LS_OPTIONS="${LS_OPTIONS} -v" # natural sort of (version) numbers within text
+
+alias l="ls  ${LS_OPTIONS} --almost-all"
+alias ll="ls ${LS_OPTIONS}"
