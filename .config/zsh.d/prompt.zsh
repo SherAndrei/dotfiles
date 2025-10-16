1d46296ea408bb5f575d683bae47b21d74a4cc07
SherAndrei <welldryus@gmail.com>
2025-10-16 14:10:19 +0300

    prompt: use standard ascii symbol to be able copy easily

--- .config/zsh.d/prompt.zsh
+++ .config/zsh.d/prompt.zsh
@@ -5,10 +5,10 @@ fpath+=( "$ZSH_CONFIG_DIR"/pure )
 
 # extended ascii symbols instead of default UTF-8
 # source: https://github.com/sindresorhus/pure/wiki/Customizations,-hacks-and-tweaks#safer-symbols
-PURE_PROMPT_SYMBOL='»'
+PURE_PROMPT_SYMBOL='$'
 PURE_GIT_DOWN_ARROW='↓'
 PURE_GIT_UP_ARROW='↑'
-PURE_PROMPT_VICMD_SYMBOL='«'
+PURE_PROMPT_VICMD_SYMBOL='<'
 
 # xterm-256color system blue (bold blue as in `ls` directory color '\e[01;34m')
 # source: https://www.ditig.com/256-colors-cheat-sheet
