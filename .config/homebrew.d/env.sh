d1815f56aaf28ad339e06b2c5840faae9922b707
SherAndrei <welldryus@gmail.com>
2026-07-13 23:38:01 +0300

    brew: disable autoupdates

--- .config/homebrew.d/env.sh
+++ .config/homebrew.d/env.sh
@@ -2,3 +2,6 @@
 # source: https://docs.brew.sh/Homebrew-on-Linux
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Disable autoupdates since they are useless most of the times
+export HOMEBREW_NO_AUTO_UPDATE=1
